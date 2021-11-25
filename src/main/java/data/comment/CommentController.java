03334ff16e4446081ff589186a793f8028702869
wonseopK <wonseop77@daum.net>
2021-11-25 17:08:15 +0900

    ws complete making comment structure with base crud functions
    added
     -CommentController
     -CommentDto
     -CommentMapper
     -CommentService
     -CommentSql
     -projectCommunity.jsp
    function detalis
     -if grph is 0 just insert comment
     -if grph is not 0 reply comment
     -get comment list
     -update comment
     -delete comment depends on the value of the grph
     -update the value of the fix colum

---
+++ src/main/java/data/comment/CommentController.java
@@ -0,0 +1,68 @@
+package data.comment;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CommentController {
+	@Autowired
+	CommentService commentService;
+	
+	@ResponseBody
+	@PostMapping("/comment/insert")
+	public void insert(CommentDTO commentDTO) {
+		System.out.println("댓글등록" + commentDTO.getContent());
+		commentDTO.setGrp(commentService.select_maxNum()+1);
+		commentService.insert_comment(commentDTO);
+	}
+	
+	@ResponseBody
+	@PostMapping("/comment/reply")
+	public void reply(CommentDTO commentDTO) {
+		commentService.change_hierarchy(commentDTO.getGrp(), commentDTO.getGrph());
+		commentDTO.setGrph(commentDTO.getGrph()+1);
+		commentDTO.setGrps(commentDTO.getGrps()+1);
+		commentService.insert_comment(commentDTO);
+		int num = commentService.select_maxNum();
+		commentService.update_parent(commentDTO.getParent(), String.valueOf(num));
+	}
+	
+	@ResponseBody
+	@GetMapping("/comment/list")
+	public List<CommentDTO> list2() {
+		List<CommentDTO> list = commentService.get_comment_list();
+		return list;
+	}
+	@ResponseBody
+	@PostMapping("/comment/delete")
+	public void delete(String num, String grp, String grph) {
+		if(Integer.parseInt(grph) != 0) {
+			commentService.delete_comment(num);
+		}else {
+			commentService.delete_branch_comment(grp);
+		}
+	}
+	@ResponseBody
+	@PostMapping("/comment/update")
+	public void delete(String num, String comment) {
+		commentService.update_comment(comment, num);
+	}
+	
+	@ResponseBody
+	@PostMapping("/comment/fix")
+	public void fix(String num) {
+		commentService.rest_fix();
+		commentService.fix_comment(num);
+	}
+	@ResponseBody
+	@PostMapping("/comment/cancelFix")
+	public void cancelFix(String num) {
+		commentService.cancel_fix(num);
+	}
+}
---
+++ src/main/java/data/comment/CommentMapper.java
@@ -0,0 +1,20 @@
+package data.comment;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommentMapper {
+	public void insert_comment(CommentDTO CMDTO);
+	public int select_maxNum();
+	public List<CommentDTO> get_comment_list();
+	public void change_hierarchy(int grp, int grph);
+	public void update_parent(String parent, String num);
+	public void update_comment(String content, String num);
+	public void delete_comment(String num);
+	public void delete_branch_comment(String idx);
+	public void rest_fix();
+	public void fix_comment(String num);
+	public void cancel_fix(String num);
+}
