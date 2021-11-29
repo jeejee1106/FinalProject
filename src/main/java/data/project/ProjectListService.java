03a64b4fa5709d2982baf35df1e947895c019aec
kyu min Park <pkm1614@google.com>
2021-11-29 18:10:16 +0900

    km connect

--- src/main/java/data/project/ProjectListService.java
+++ src/main/java/data/project/ProjectListService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -15,6 +17,7 @@ public class ProjectListService {
 	
 	public List<ProjectDTO> getAllProjects(String category,String state,String percent)
 	{
+//		System.out.println(search);
 		return mapper.getAllProjects(category,state,percent);
 	}
 }
