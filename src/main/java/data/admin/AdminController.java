6e3bca7efd7834a0cd80afa67634fc4d2fef3694
jiwon1114_1 <jiwon8518@naver.com>
2021-12-03 14:20:41 +0900

    jw_admin
    프로젝트 승인관리

--- src/main/java/data/admin/AdminController.java
+++ src/main/java/data/admin/AdminController.java
@@ -7,10 +7,15 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import data.member.MemberDTO;
 import data.member.MemberService;
+import data.profile.ProfileService;
 import data.project.ProjectDTO;
 
 
@@ -21,13 +26,14 @@ public class AdminController {
 	AdminService service;
 	@Autowired
 	MemberService memberSerivce;
+	@Autowired
+	ProfileService profileSerivce;
 	
 	@GetMapping("/admin/project_management")
 	public ModelAndView projectList(HttpSession session) {
 		
 		ModelAndView mview = new ModelAndView();
 		String id = (String) session.getAttribute("id");
-		String name = memberSerivce.getName(id);
 		MemberDTO dto = memberSerivce.getAll(id);
 		
 		List<ProjectDTO> list = service.getProjectList();
@@ -35,8 +41,37 @@ public class AdminController {
 		
 		mview.addObject("dto", dto);
 		mview.addObject("list", list);
-		mview.setViewName("/profile/projectManagement");
+		mview.setViewName("/admin/projectManagement");
 		
 		return mview;
 	}
+	
+	@GetMapping("/admin/project_aprvl")
+	@ResponseBody
+	public void updateAprvl(@ModelAttribute ProjectDTO pdto) {
+		
+		service.updateAuditAprvl(pdto);
+	}
+	
+	@GetMapping("/admin/project_refusal")
+	@ResponseBody
+	public void updateRefusal(@ModelAttribute ProjectDTO pdto) {
+		
+		service.updateAuditRefusal(pdto);
+	}
+	
+	@GetMapping("/admin/audit_detail")
+	@ResponseBody
+	public ModelAndView auditDetail (@RequestParam String idx) {
+		
+		ModelAndView mview = new ModelAndView();
+		ProjectDTO pdto = profileSerivce.getProject(idx);
+		
+		mview.addObject("pdto", pdto);
+		mview.setViewName("/admin/auditDetail");
+		
+		return mview;
+	}
+	
+	
 }
