89716fbc8804da0bbd7f21f709eb7403d8e28d12
minMin <goals1005@naver.com>
2021-12-12 13:50:10 +0900

    HM header 수정완료했습니다.

--- src/main/java/data/project/ProjectController.java
+++ src/main/java/data/project/ProjectController.java
@@ -106,7 +106,7 @@ public class ProjectController {
 	@PostMapping("/project/defaultUpdate")
 	public String defaultUpdate(@ModelAttribute ProjectDTO dto,HttpServletRequest request) {
 
-		String path = request.getSession().getServletContext().getRealPath("/file");
+		String path = request.getSession().getServletContext().getRealPath("/thumbnail_image");
 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
 
